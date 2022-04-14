d16baeafdce397e8d574cfe746ac9761222571a8
Razzkz <sdearaujo.rodrigo@gmail.com>
2022-04-14 12:35:58 -0300

    Atualization ex015

--- aula12/ex015/alert/script-modelo.js
+++ aula12/ex015/alert/script-modelo.js
@@ -9,11 +9,41 @@ function verificar() {
         var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
         var genero = ''
-        if(fsex[0].checked) {
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked) {
             genero = 'Homem'
+            if (idade >= 0 && idade < 10) {
+                // Criança
+                img.setAttribute( 'src', 'Menino.JPG.jpg')
+            } else if (idade < 21) {
+                // Adolescente
+                img.setAttribute('src', 'AdolescenteM.JPG.jpg')
+            } else if (idade < 50) {
+                // Adulto
+                img.setAttribute('src', 'Homem.JPG.jpg')
+            } else {
+                // Idoso 
+                img.setAttribute('src', 'Idoso.JPG.jpg')
+            }
         } else if (fsex[1].checked) {
             genero = 'Mulher'
+            if (idade >= 0 && idade < 10) {
+                // Criança
+                img.setAttribute('src', 'Menina.JPG.jpg')
+            } else if (idade < 21) {
+                // Adolescente
+                img.setAttribute('src', 'AdolescenteM.JPG.jpg')
+            } else if (idade < 50) {
+                // Adulto
+                img.setAttribute('src', 'Mulher.JPG.jpg')
+            } else {
+                // Idosa
+                img.setAttribute('src', 'Idosa.JPG.jpg')
+            }
+            res.style.textAlign = 'center'
+            res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+            res.appendChild(img)
         }
-        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
     }
 }
